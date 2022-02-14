ebd976dccbf8b80956b146cbe95d9c772ceb93f6
Leonardo FG <leonardo.fg@engineer.com>
2022-02-14 01:38:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /* Regras Codificador: 
 "e" é convertido para "enter" 
 "i" é convertido para "imes"
@@ -10,27 +9,22 @@ Não permite acentuação
 */
 var entradaCodificada = '';
 var saidaDescriptografada = '';
-var textoResultado = '';
-var entrada = '';
-//entrada.value;
-//var entrada = 'aeiou';
+var entrada = document.querySelector('#input-texto');
+var textoResultado = document.querySelector('#msg');
 
 function criptografar(){
     event.preventDefault(); //para não carregar a pagina de no após clicar e limpar os campos
-    entrada = document.querySelector('#input-texto').value;
-    entradaCodificada = entrada.replace(/[e\é\ê]/gi, 'enter')
+    //entrada = document.querySelector('#input-texto').value;
+    entradaCodificada = entrada.value.replace(/[e\é\ê]/gi, 'enter')
     .replace(/[i\í\î]/gi, 'imes').
     replace(/[a\á\â\ã\á]/gi, 'ai').
     replace(/[o\ó\õ\ô\ó]/gi, 'ober').
     replace(/[u\ú\ú]/gi, 'ufat');
     console.log(entradaCodificada);
-
-    textoResultado = document.querySelector('#msg');
-
+    
     textoResultado.value = entradaCodificada;
 
 }
-
 
 /* Regras Decodificador: 
 "enter" é convertido para "e" 
@@ -46,14 +40,16 @@ var palavrasChave = ['enter', 'imes', 'ai', 'ober', 'ufat']
 
 function descriptografar(){
     event.preventDefault();
-    var verificar = palavrasChave.some(elementoArray => entradaCodificada.includes(elementoArray));
+    var verificar = palavrasChave.some(elementoArray => entrada.value.includes(elementoArray));
     if (verificar) {
-        saidaDescriptografada = textoResultado.replace('enter', 'e')
-        .replace('imes', 'i' ).
-        replace('ai', 'a').
-        replace('ober', 'o').
-        replace('ufat', 'u'); 
+        saidaDescriptografada = entrada.value.replace('enter', 'e')
+            .replace('imes', 'i' )
+                .replace('ai', 'a')
+                    .replace('ober', 'o')
+                        .replace('ufat', 'u'); 
         console.log(saidaDescriptografada);
+        textoResultado.value = saidaDescriptografada;
+        
         } else {
         alert('A mensagem não está criptografada');
     }
@@ -71,13 +67,13 @@ ElementoBotaoCopy.addEventListener('click', copiaTexto);
 
 
 function copiaTexto(){
-    var texto = document.querySelector('#msg');
+    
+    navigator.clipboard.writeText(textoResultado.value);
 
-    navigator.clipboard.writeText(texto.value);
+    // Podemos ainda limpar a caixa de textoResultado, atribuindo o valor '' ao elemento: 
 
-    // Podemos ainda limpar a caixa de texto, atribuindo o valor '' ao elemento: 
-
-    texto.value = '';
+    textoResultado.value = '';
+    entrada.value = '';
     var campoEntrada = document.querySelector('#input-texto');
     campoEntrada.focus();
     console.log('copiaTexto');
