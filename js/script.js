967a71cc99b1253b83847d24ca7ac40119821b07
Leonardo FG <leonardo.fg@engineer.com>
2022-02-13 22:21:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,22 @@ Apenas letras minúsculas
 Não permite acentuação   
 */
 
+//var entrada = document.querySelector('.text-input').value;
+//entrada.value;
+var entrada = 'aeiou';
+
+var entradaCodificada = entrada.replace(/[e\é\ê]/gi, 'enter')
+.replace(/[i\í\î]/gi, 'imes').
+replace(/[a\á\â\ã\á]/gi, 'ai').
+replace(/[o\ó\õ\ô\ó]/gi, 'ober').
+replace(/[u\ú\ú]/gi, 'ufat');
+console.log(entradaCodificada);
+
+var textoResultado = document.querySelector('.text-input');
+
+textoResultado.value = entradaCodificada;
+
+
 /* Regras Decodificador: 
 "enter" é convertido para "e" 
 "imes" é convertido para "i"
