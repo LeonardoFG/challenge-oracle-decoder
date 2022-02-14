d196d42d7e61d33ca15b5142ae2ee019b75b195f
Leonardo FG <leonardo.fg@engineer.com>
2022-02-13 23:45:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,13 +8,15 @@
 Apenas letras minúsculas
 Não permite acentuação   
 */
-
-//var entrada = document.querySelector('.text-input').value;
-//entrada.value;
-var entrada = 'aeiou';
+var entradaCodificada = '';
+var saidaDescriptografada = '';
+var textoResultado = '';
+var entrada = document.querySelector('.text-input').value;
+entrada.value;
+//var entrada = 'aeiou';
 
 function criptografar(){
-    var entradaCodificada = entrada.replace(/[e\é\ê]/gi, 'enter')
+    entradaCodificada = entrada.replace(/[e\é\ê]/gi, 'enter')
     .replace(/[i\í\î]/gi, 'imes').
     replace(/[a\á\â\ã\á]/gi, 'ai').
     replace(/[o\ó\õ\ô\ó]/gi, 'ober').
@@ -39,48 +41,49 @@ Não permite acentuação
 */
 
 var palavrasChave = ['enter', 'imes', 'ai', 'ober', 'ufat']
-var verificar = palavrasChave.some(elementoArray => entradaCodificada.includes(elementoArray));
 
-if (verificar) {
-    descriptografar();
-    console.log(saidaDescriptografada);
-} else {
-	alert('A mensagem não está criptografada');
-}
 
 function descriptografar(){
-    var saidaDescriptografada = textoResultado.replace('enter', 'e')
-    .replace('imes', 'i' ).
-    replace('ai', 'a').
-    replace('ober', 'o').
-    replace('ufat', 'u'); 
-    console.log(saidaDescriptografada);
+    var verificar = palavrasChave.some(elementoArray => entradaCodificada.includes(elementoArray));
+    if (verificar) {
+        saidaDescriptografada = textoResultado.replace('enter', 'e')
+        .replace('imes', 'i' ).
+        replace('ai', 'a').
+        replace('ober', 'o').
+        replace('ufat', 'u'); 
+        console.log(saidaDescriptografada);
+        console.log(saidaDescriptografada);
+    } else {
+        alert('A mensagem não está criptografada');
+    }
+
 }
 
 var ElementoBotaoC = document.querySelector('#btn-cripto');
 
-ElementoBotaoC.addEventListener('click', criptografar());
+ElementoBotaoC.addEventListener('click', criptografar);
 
 var ElementoBotaoD = document.querySelector('#btn-descripto');
 
-ElementoBotaoD.addEventListener('click', descriptografar());
+ElementoBotaoD.addEventListener('click', descriptografar);
 
-input-texto.addEventListener('click', function () {
+var ElementoBotaoCopy = document.querySelector('#btn-copy');
+
+ElementoBotaoCopy.addEventListener('click', copiaTexto);
+
+inputtexto.addEventListener('click', function copiaTexto() {
 	// linhas de código...
 })
 
-// ou
 
-input-texto.textContent = 'blablabla'
-input-texto.value = 'taltaltal'
 
 
 function copiaTexto(){
-    var texto = document.querySelector('.texto-class')
+    var texto = document.querySelector('#msg');
 
-    navigator.clipboard.writeText(texto.value)
+    navigator.clipboard.writeText(texto.value);
 
     // Podemos ainda limpar a caixa de texto, atribuindo o valor '' ao elemento: 
 
-    texto.value = ''
+    texto.value = '';
 }
